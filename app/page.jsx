fbc80a511b03648c22f36a5b09eb52a57c59a544
tanishqawasthi <awasthitanishq03@gmail.com>
2023-08-08 21:53:14 +0530

    navbar

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,20 @@
+import Feed from "@components/Feed"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover and Share
+            <br className="max-md:hidden" />
+            <span className="orange_gradient text-center"> AI Powered Prompts</span>
+        </h1>
+        <p className="desc text-center">
+            Promptopia is an AI Prompting tool for the modern world to discover, create and share creative prompts.
+        </p>
+
+        {/* Feed */}
+        <Feed />
+    </section>
   )
 }
 
